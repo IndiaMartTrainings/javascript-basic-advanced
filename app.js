af35b9048fa91bdbeba3039932c79c73ead109b7
UmamaheshMaxwell <maheshmeka16@gmail.com>
2022-01-20 14:57:09 +0530

    added es6 basics

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var a =10;
+// var a =10;
 
 
-function sample(){
-    var a =20;
-}
+// function sample(){
+//     var a =20;
+// }
 
-console.log(a)
+// console.log(a)
+
+var div = document.getElementById("header")
+console.log(div.innerText)
+
+var para = document.getElementsByClassName('para')
+console.log(para[0].textContent)
